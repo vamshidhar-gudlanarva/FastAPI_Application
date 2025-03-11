0f8ad0466e3a83675962a26271556ec46d1df9fa
vamshidhar <vamshidhar1001@gmail.com>
2025-03-11 14:53:57 -0700

    created todos.db

--- ToDoApp/main.py
+++ ToDoApp/main.py
@@ -1,9 +1,23 @@
-from fastapi import FastAPI
+from typing import Annotated
+from sqlalchemy.orm import Session
+from fastapi import FastAPI, Depends
 import models
-from database import engine
+from models import Todos
+from database import engine, SessionLocal
 
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get("/")
+async def read_all(db: Annotated[Session, Depends(get_db)]):
+    return db.query(Todos).all()
